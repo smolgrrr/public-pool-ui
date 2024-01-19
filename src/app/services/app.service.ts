fcfc541cfb3b92cd48c29b5c2d34175faad00367
smolgrrr <smolgrrr@protonmail.com>
2024-01-19 23:24:50 +1100

    rm ghcr

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
     providedIn: 'root'
 })
@@ -12,12 +14,12 @@ export class AppService {
     ) { }
 
     public getInfo() {
-        return this.httpClient.get(`/api/info`) as Observable<any>;
+        return this.httpClient.get(`http://${window.location.hostname}:2019/api/info`) as Observable<any>;
     }
     public getNetworkInfo() {
-        return this.httpClient.get(`/api/network`) as Observable<any>;
+        return this.httpClient.get(`http://${window.location.hostname}:2019/api/network`) as Observable<any>;
     }
     public getInfoChart() {
-        return this.httpClient.get(`/api/info/chart`) as Observable<any>;
+        return this.httpClient.get(`http://${window.location.hostname}:2019/api/info/chart`) as Observable<any>;
     }
 }
--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
